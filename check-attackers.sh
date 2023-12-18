af7484d243d2caa3070aa23368515e360c58642b
UltimateByte <git@lrob.fr>
2023-12-18 16:55:30 +0100

    Update check-attackers.sh
    Added email capability and improvements

--- check-attackers.sh
+++ check-attackers.sh
@@ -6,6 +6,15 @@
 # Settings
 sleep_time="1" # Used to avoid rate-limiting
 whois_timeout="5" # Timeout for whois queries in seconds
+send_abuse_emails="off" # on/off # WARNING: may expose sensitive information especially when including ssh and apache logs.
+abuse_email_sender="abuse@yourdomain.tld" # You shall change to a real email address with an SPF rule authenticating your sender
+# Note log paths are valid for a Plesk server - tested on Debian only
+include_fail2ban_log="on" # on/off
+fail2ban_logpath='/var/log/fail2ban.log'
+include_apache_log="off" # on/off
+apache_logpath='/var/www/vhosts/*/logs/*log'
+include_ssh_log="off" # on/off
+ssh_logpath='/var/log/auth.log'
 
 # Get the name of this script for logging purposes
 selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
@@ -24,6 +33,7 @@ fi
 source ultimate-bash-api.sh
 
 declare -A ip_counts
+declare -A abuse_email_ips
 total_ips=0
 
 function fetch_whois_data {
@@ -83,6 +93,9 @@ function process_ip {
     ((ip_counts["${org_name}"]++))
     ((total_ips++))
 
+    # Collect IPs for each abuse email
+    abuse_email_ips[${abuse_email}]+="${ip} "
+
     # Verbose Output and Logging
     fn_logecho "${ip} - ${org_name} - Abuse Email: ${abuse_email}"
 }
@@ -96,19 +109,65 @@ done
 # Output statistics
 fn_logecho "Provider IP Statistics:"
 
-# Iterate over the array keys (organization names) and print them along with their counts
 for org in "${!ip_counts[@]}"; do
-    printf "%s|%s\n" "$org" "${ip_counts[$org]}"
-done | sort -t'|' -k2nr | while IFS='|' read -r org count; do
-    if [[ -n "${count}" ]] && [[ "${total_ips}" -gt 0 ]] && [[ "${count}" -gt 0 ]]; then
+    if [[ -n "${ip_counts[$org]}" ]] && [[ "${total_ips}" -gt 0 ]] && [[ "${ip_counts[$org]}" -gt 0 ]]; then
         if command -v bc &> /dev/null; then
-            percentage=$(bc <<< "scale=2; ${count}*100/${total_ips}")
-            stat_output="${org}: ${count} IPs (${percentage}%)"
+            percentage=$(bc <<< "scale=2; ${ip_counts[$org]}*100/${total_ips}")
+            stat_output="${org}: ${ip_counts[$org]} IPs (${percentage}%)"
         else
-            stat_output="${org}: ${count} IPs"
+            stat_output="${org}: ${ip_counts[$org]} IPs"
         fi
     else
-        stat_output="${org}: ${count:-0} IPs (N/A)"
+        stat_output="${org}: ${ip_counts[$org]:-0} IPs (N/A)"
     fi
     fn_logecho "${stat_output}"
 done
+
+function fetch_fail2ban_log {
+    local ip=$1
+    grep "${ip}" "${fail2ban_logpath}"
+}
+
+function fetch_apache_log {
+    local ip=$1
+    grep -rnw "${ip}" "${apache_logpath}"
+}
+
+function fetch_ssh_log {
+    local ip=$1
+    grep -rnw "${ip}" "${ssh_logpath}"
+}
+
+# Send report for each abuse email
+if [ "${send_abuse_emails}" == "on" ]; then
+    fn_logecho "Sending abuse emails..."
+    for email in "${!abuse_email_ips[@]}"; do
+        if [[ "${email}" != "Unknown" ]]; then
+            mailsubject="Abuse Report - Your network is attacking ${HOSTNAME}"
+            mailcontent="From: ${abuse_email_sender}\n"  # Add your "From" email here
+            mailcontent+="To: ${email}\n"
+            mailcontent+="Subject: ${mailsubject}\n"
+            mailcontent+="Report for Abuse Email: ${email}\n"
+            
+            for ip in ${abuse_email_ips[${email}]}; do
+                mailcontent+="\nIP: ${ip}\n"
+                if [[ "${include_fail2ban_log}" == "on" ]]; then
+                    mailcontent+="\nFail2Ban Log Entries:\n$(fetch_fail2ban_log "${ip}")\n"
+                fi
+                if [[ "${include_apache_log}" == "on" ]]; then
+                    mailcontent+="\nApache Log Entries:\n$(fetch_apache_log "${ip}")\n"
+                fi
+                if [[ "${include_ssh_log}" == "on" ]]; then
+                    mailcontent+="\nSSH Log Entries:\n$(fetch_ssh_log "${ip}")\n"
+                fi
+            done
+            # Actually send the mail
+            echo -e "${mailcontent}" | mail -s "${mailsubject}" "${email}"
+            # Log and display what is sent
+            fn_logecho "Sending Mail From: ${abuse_email_sender}" ; fn_logecho "Subject: ${mailsubject}" ; fn_logecho "To be sent to: ${email}" ; fn_logecho -e "${mailcontent}"
+        fi
+    done
+fi
+
+# Display script exec duration using Ultimate BASH API
+fn_duration
