ee2e5ee542cdfb9e31f63abcbd14c8bc70de978a
UltimateByte <git@lrob.fr>
2023-12-18 13:47:42 +0100

    Update check-attackers.sh
    Stats fix & local variables improvements

--- check-attackers.sh
+++ check-attackers.sh
@@ -5,6 +5,7 @@
 
 # Settings
 sleep_time="1" # Used to avoid rate-limiting
+whois_timeout="5" # Timeout for whois queries in seconds
 
 # Get the name of this script for logging purposes
 selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
@@ -27,12 +28,13 @@ total_ips=0
 
 function fetch_whois_data {
     local ip=$1
-    whois "${ip}"
+    timeout "${whois_timeout}" whois "${ip}"
 }
 
 function extract_org_name {
     local whois_data=$1
-    local org_name=$(echo "${whois_data}" | grep -i "OrgName" | awk -F: '{print $2}' | xargs)
+    local org_name
+    org_name=$(echo "${whois_data}" | grep -i "OrgName" | awk -F: '{print $2}' | xargs)
     
     if [[ -z "${org_name}" ]]; then
         org_name=$(echo "${whois_data}" | grep -i "netname" | awk -F: '{print $2}' | xargs)
@@ -54,7 +56,8 @@ function extract_org_name {
 
 function extract_abuse_email {
     local whois_data=$1
-    local abuse_email=$(echo "${whois_data}" | grep -i "abuse" | grep "@" | awk '{print $NF}' | head -n 1)
+    local abuse_email
+    abuse_email=$(echo "${whois_data}" | grep -i "abuse" | grep "@" | awk '{print $NF}' | head -n 1)
     
     if [[ -z "${abuse_email}" ]]; then
         abuse_email=$(echo "${whois_data}" | grep -i "e-mail" | awk '{print $NF}' | head -n 1)
@@ -66,12 +69,15 @@ function extract_abuse_email {
     echo "${abuse_email:-Unknown}"
 }
 
-
 function process_ip {
     local ip=$1
-    local whois_data=$(fetch_whois_data "${ip}")
-    local org_name=$(extract_org_name "${whois_data}")
-    local abuse_email=$(extract_abuse_email "${whois_data}")
+    local whois_data
+    local org_name
+    local abuse_email
+
+    whois_data=$(fetch_whois_data "${ip}")
+    org_name=$(extract_org_name "${whois_data}")
+    abuse_email=$(extract_abuse_email "${whois_data}")
 
     # Count IPs per Organization
     ((ip_counts["${org_name}"]++))
@@ -89,12 +95,13 @@ done
 
 # Output statistics
 fn_logecho "Provider IP Statistics:"
-for org in $(printf "%s\n" "${!ip_counts[@]}" | sort -nr | while read -r count org; do
+for org in $(printf "%s\n" "${!ip_counts[@]}" | sort -nr); do
+    count="${ip_counts[$org]}"
     if command -v bc &> /dev/null; then
-        percentage=$(bc <<< "scale=2; $count*100/$total_ips")
-        stat_output="$org: $count IPs ($percentage%)"
+        percentage=$(bc <<< "scale=2; ${count}*100/${total_ips}")
+        stat_output="${org}: ${count} IPs (${percentage}%)"
     else
-        stat_output="$org: $count IPs"
+        stat_output="${org}: ${count} IPs"
     fi
-    fn_logecho "$stat_output"
+    fn_logecho "${stat_output}"
 done
